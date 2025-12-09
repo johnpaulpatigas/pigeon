eab5a5e5bada2391dabfa45d16345ac8eb01000d
johnpaulpatigas <johnpaulpatigas@gmail.com>
2025-12-09 22:20:18 +0800

    feat(native): Disable webview overscroll and scrollbars

--- android/app/src/main/java/com/johnpaulpatigas/pigeon/MainActivity.java
+++ android/app/src/main/java/com/johnpaulpatigas/pigeon/MainActivity.java
@@ -1,5 +1,18 @@
 package com.johnpaulpatigas.pigeon;
 
+import android.os.Bundle;
+import android.webkit.WebView;
+
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        WebView webview = getBridge().getWebView();
+        webview.setOverScrollMode(WebView.OVER_SCROLL_NEVER);
+        webview.setVerticalScrollBarEnabled(false);
+        webview.setHorizontalScrollBarEnabled(false);
+    }
+}
